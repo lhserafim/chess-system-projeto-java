ff2d8822fc6db2734d61dd7a94349da3ca1854fc
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-05-29 08:14:07 -0300

    imprime o alfabeto no cabeçalho

--- src/application/UI.java
+++ src/application/UI.java
@@ -78,6 +78,7 @@ public class UI {
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
+        System.out.println("  a b c d e f g h"); // imprime o alfabeto no cabeçalho
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " "); // Imprime a numeração lateral
             for (int j = 0; j < pieces.length; j++) {
@@ -90,6 +91,7 @@ public class UI {
 
     // trabalhando com sobrecarga
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+        System.out.println("  a b c d e f g h"); // imprime o alfabeto no cabeçalho
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " "); // Imprime a numeração lateral
             for (int j = 0; j < pieces.length; j++) {
